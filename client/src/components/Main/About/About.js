d32d890226c9c28f55f45d9078ba9570b76be2f0
joejhansen <josephjameshansen@gmail.com>
2022-12-12 16:07:07 -0800

    drafting copy for info and about

--- client/src/components/Main/About/About.js
+++ client/src/components/Main/About/About.js
@@ -1,7 +1,139 @@
+import './About.css'
 const About = () => {
     return (
-        <div className="container">
-        <p>About</p>
+        <div id="pageWrapper" className='container-fluid'>
+            <div id="info-section" className="row">
+                <div id="info-buffer" className="col">
+                    <div id="info-slate" className="row">
+                        <div id="info-main" className="col">
+                            {/* <div className="row info-columns">
+                                <div className="col info-column popout">
+
+                                    <div className="info-column-head">
+                                        <p>The Platform</p>
+                                    </div>
+                                    <div className="info-column-content">
+                                        <p id="top-column-text">Find out what powers Option Select Database</p>
+                                    </div>
+                                </div>
+                            </div> */}
+
+                            <div className="row mid-banner">
+                                <div className="col mid-banner-buffer">
+                                    <p className="mid-banner-info">Slippi Melee</p>
+                                </div>
+                            </div>
+                            <div className="row info-columns">
+                                <div className="col-md-6 write-share-go">
+                                    <div className='column-wrapper'>
+                                        <div className="row info-write">
+                                            <div className="col info-column popout">
+                                                <p className="info-column-head">Slippi-JS</p>
+                                                <div className="info-column-content">
+                                                    <p>&emsp;Option Select parses .slp files with Slippi-JS and formats them into a searchable model</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="row info-share">
+                                            <div className="col info-column popout">
+                                                <p className="info-column-head">React/Express</p>
+                                                <div className="info-column-content">
+                                                    <p>&emsp;Smoothly nagivate the site using React Router and upload files with Multer and Express.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="row info-go">
+                                            <div className="col info-column popout">
+                                                <p className="info-column-head">ApolloGQL</p>
+                                                <div className="info-column-content">
+                                                    <p>&emsp;Connect from anywhere for anything using a URI key.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-6 popout column-wrapper">
+                                    <div className=' info-column '>
+                                        <p className="info-column-head">Special Thanks</p>
+                                        <div className="info-column-content">
+                                            <p>&emsp;Option Select is built with the MERN stack but would not be possible let alone conceivable without the dedicated work of Jas "Fizzi" Laferriere, Vince Au, Nikhil "Nikki" Narayana, and all the other contributors to Slippi-JS.</p>
+                                            <br />
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <div className="row mid-banner">
+                                <div className="col mid-banner-buffer">
+                                    <p className="mid-banner-info">Expanding Functionality</p>
+                                </div>
+                            </div>
+                            <div className="row info-columns">
+                                <div className="col-md-6 popout column-wrapper">
+                                    <div className=' info-column '>
+                                        <p className="info-column-head">The FGC</p>
+                                        <div className="info-column-content">
+                                            <p>&emsp;If this can be done with a 20 year old game, who's to say it can't be done for the latest releases and their own legacy titles?</p>
+                                            <p>&emsp;</p>
+                                            <p>&emsp;</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 write-share-go">
+                                    <div className='column-wrapper'>
+                                        <div className="row info-write">
+                                            <div className="col info-column popout">
+                                                <p className="info-column-head">Any Game</p>
+                                                <div className="info-column-content">
+                                                    <p>&emsp;Street Fighter, Tekken, Killer Instinct, BlazBlue, hopefully one day even Dive Kick.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="row info-share">
+                                            <div className="col info-column popout">
+                                                <p className="info-column-head">Anywhere</p>
+                                                <div className="info-column-content">
+                                                    <p>&emsp;Use the API to search for anything, anywhere, anytime</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="row info-go">
+                                            <div className="col info-column popout">
+                                                <p className="info-column-head">Anyone</p>
+                                                <div className="info-column-content">
+                                                    <p>&emsp;No secrets, no red tape, no freemium bullshit. This app wouldn't be possible without the work of people a lot smarter than myself. If the information is there, you should have access to it.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            {/* <div className="row mid-banner">
+                                <div className="col mid-banner-buffer">
+                                    <p className="mid-banner-info">TL;DR</p>
+                                </div>
+                            </div>
+                            <div className='column-wrapper'>
+
+                                <div className="row info-columns">
+                                    <div className="col info-column popout">
+                                        <div className="info-column-head">
+                                            <p>Upload and Test It Out</p>
+                                        </div>
+                                        <div className="info-column-content">
+                                            <p></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> */}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/components/Main/Home/Info/Info.js
+++ client/src/components/Main/Home/Info/Info.js
@@ -8,12 +8,13 @@ const Info = () => {
                         <div id="info-main" className="col">
                             <div className="row info-columns">
                                 <div className="col info-column popout">
+
                                     <div className="info-column-head">
                                         <p>Welcome to</p>
                                         <p>Option Select</p>
                                     </div>
                                     <div className="info-column-content">
-                                        <p id="top-column-text">logline</p>
+                                        <p id="top-column-text">"An Option Select is a tactic in which a player takes advantage of certain input priority systems to cover multiple different scenarios at once from the same input." - Street Fighter Wiki</p>
                                     </div>
                                 </div>
                             </div>
@@ -23,94 +24,67 @@ const Info = () => {
                                 </div>
                             </div>
                             <div className="row info-columns">
-                                <div className="col info-column popout">
-                                    <p className="info-column-head">Mission</p>
-                                    <div className="info-column-content">
-                                        <p>&emsp;</p>
-                                        <br />
+                                <div className="col-md-6 popout column-wrapper">
+                                    <div className=' info-column '>
+                                        <p className="info-column-head">Mission</p>
+                                        <div className="info-column-content">
+                                            <p>&emsp;I grew up playing fighting games with my friends, having a blast while executing combos and kicking ass. As I got older, my appreciation of these games grew and I wanted to improve my gameplay and results at local tournaments.</p>
+                                            <p>&emsp;However, it's hard to get a sense of your holistic improvement as a player without data to back up your perceptions. Each game has its own UX that makes it difficult to compare data, if it even has that data in the first place.</p>
+                                            <p>&emsp;My goal with the Option Select Database is to create a semi-centralized, open-source database for fighting game replays and analytics available through the we app or its API, starting with Super Smash Bros: Melee.</p>
+                                        </div>
+
                                     </div>
                                 </div>
-                                <div className="col write-share-go">
-                                    <div className="row info-write">
-                                        <div className="col info-column popout">
-                                            <p className="info-column-head">Upload</p>
-                                            <div className="info-column-content">
-                                                <p>&emsp;</p>
+                                <div className="col-md-6 write-share-go">
+                                    <div className='column-wrapper'>
+                                        <div className="row info-write">
+                                            <div className="col info-column popout">
+                                                <p className="info-column-head">.slp Upload</p>
+                                                <div className="info-column-content">
+                                                    <p>&emsp;Upload your Slippi Melee files and allow for the server to parse and analyze your games using Slippi-JS</p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className="row info-share">
-                                        <div className="col info-column popout">
-                                            <p className="info-column-head">Search</p>
-                                            <div className="info-column-content">
-                                                <p>&emsp;</p>
+                                        <div className="row info-share">
+                                            <div className="col info-column popout">
+                                                <p className="info-column-head">Search</p>
+                                                <div className="info-column-content">
+                                                    <p>&emsp;Search individual Connect Codes, Display Names, and games or get a full list of everything. There's no red tape or secrets: iff the file has it, the database has it too.</p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className="row info-go">
-                                        <div className="col info-column popout">
-                                            <p className="info-column-head">Analyze</p>
-                                            <div className="info-column-content">
-                                                <p>&emsp;</p>
+                                        <div className="row info-go">
+                                            <div className="col info-column popout">
+                                                <p className="info-column-head">Analyze</p>
+                                                <div className="info-column-content">
+                                                    <p>&emsp;Analyze stats available through the Slippi Launcher with more analytics such as Head-to-Head matchups coming soon.</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+
                             </div>
-                            {/* <div className="row mid-banner">
-                                <div className="col mid-banner-buffer">
-                                    <p className="mid-banner-info">Current Functionality</p>
-                                </div>
-                            </div>
-                            <div className="row info-columns">
-                                <div className="col write-share-go">
-                                    <div className="row info-write">
-                                        <div className="col info-column popout">
-                                            <p className="info-column-head">Slippi Melee</p>
-                                            <div className="info-column-content">
-                                                <p>&emsp;</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row info-share">
-                                        <div className="col info-column popout">
-                                            <p className="info-column-head"></p>
-                                            <div className="info-column-content">
-                                                <p>&emsp;</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row info-go">
-                                        <div className="col info-column popout">
-                                            <p className="info-column-head">Upload and Analyze</p>
-                                            <div className="info-column-content">
-                                                <p>&emsp;</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col info-column popout">
-                                    <p className="info-column-head">How You Can Help</p>
-                                    <div className="info-column-content">
-                                        <p>&emsp;</p>
-                                    </div>
-                                </div>
-    </div> */}
                             <div className="row mid-banner">
                                 <div className="col mid-banner-buffer">
                                     <p className="mid-banner-info">TL;DR</p>
                                 </div>
                             </div>
-                            <div className="row info-columns">
-                                <div className="col info-column popout">
-                                    <div className="info-column-head">
-                                        <p>Upload and Test It Out</p>
-                                    </div>
-                                    <div className="info-column-content">
-                                        <p></p>
+                            <div className='column-wrapper'>
+
+                                <div className="row info-columns">
+                                    <div className="col info-column popout">
+                                        <div className="info-column-head">
+                                            <p>Upload and Test It Out</p>
+                                        </div>
+                                        <div className="info-column-content">
+                                            <p id="tldr">&emsp;Upload your .slp files and search up whatever you want. The web app is in alpha currently so most of the files are my own. feel free to scrutinize my lack of improvement in 5 years of playing Melee!</p>
+                                            <p id="tldr">&emsp;And check out the GitHub (https://github.com/joejhansen/option-select-db)</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
